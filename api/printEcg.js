afa432778cab802505352fd602b190efd3d40aa8
nordexz <nordex83@gmail.com>
2023-08-15 14:45:25 +0300

    printing ecg with puppeteer

--- api/printEcg.js
+++ api/printEcg.js
@@ -1,27 +1,43 @@
-let puppeteer = require('puppeteer-core');
+let puppeteer = require('puppeteer');
 const chromium = require('chrome-aws-lambda');
 
 export default async (req, res) => {
   try {
     const browser = await puppeteer.launch({
-      args: chromium.args,
       defaultViewport: chromium.defaultViewport,
       executablePath: await chromium.executablePath,
       headless: 'new',
     });
     const page = await browser.newPage();
-    
-    await page.goto(req.body.query)
-    const pdfFIle = await page.pdf({format: 'A4'})
 
-    await browser.close();
+    console.log(req.query)
 
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', 'attachment; filename=generated-pdf.pdf');
-    res.send(pdfFIle);
-    
+    await page.goto(
+      `${req.query.site}?timestamp=${req.query.timestamp}&patientId=${req.query.patientId}&doctorName=${req.query.doctorName}&firstChart=${req.query.firstChart}&secondChart=${req.query.secondChart}`
+    );
+    await page.waitForSelector(`#${req.query.firstChart}`);
+    await page.waitForSelector(`#${req.query.secondChart}`);
+
+    const pdfFIle = await page.pdf({
+      margin: {left: 120},
+      format: "A4",
+      scale: 0.72254,
+    });
+
+    setTimeout(async () => {
+      
+      await browser.close();
+  
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader(
+        "Content-Disposition",
+        "attachment; filename=generated-pdf.pdf"
+      );
+      res.status(200);
+      res.send(pdfFIle);
+    }, 5000);
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'An error occurred' });
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred" });
   }
 };
